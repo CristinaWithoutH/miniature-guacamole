b45e01867dc4694b64a4e200208301585bc1376e
Cristina <christina.buruiana@yahoo.com>
2026-01-20 17:46:18 +0100

    pivot longer on yearly genres csv and write visualisation code for relative occurrence by birth year + lines for generations

--- visualisation.r
+++ visualisation.r
@@ -1,33 +1,24 @@
 library(tidyverse)
 
-my_data <- read_csv("writers_results.csv")
-# print(my_data)
+my_data <- read_csv("yearly_genres.csv")
+print(my_data)
 
+pivoted <- pivot_longer(my_data, c("fantasy", "science_fiction", "poetry", "satire", "observational_comedy", "non_fiction", "romance_novel"), names_to = "genre")
+print(pivoted)
 
-fantasy = filter(my_data, genre == "Fantasy")
-print(fantasy)
-
-comedy = filter(my_data, genre == "Comedy")
-print(comedy)
-
-romance = filter(my_data, genre == "Romance novel")
-
-ggplot(data = fantasy) +
-  aes (x = birth_year, fill = genre) +
-  geom_bar(fill = "magenta")
-
-# ggplot(data = romance) +
-#   aes (x = birth_year, fill = genre) +
-#   geom_bar()
-
-
-# ggplot(data = ...) +
-#   aes(x = birth_year, y = ..., color = genre) +
-#   labs(title = "Relative popularity of genres by birth year of the author",
-#       x = "Year of birth", 
-#       y = "Relative popularity", 
-#       color = "Genre") +
-#   theme_light() +
-#   scale_colour_discrete(labels = c("",)) +
-#   geom_point()
-#   geom_line()
+ggplot(data = pivoted) +
+  aes(x = year, y = value, color = genre) +
+  labs(title = "Relative occurrence of genres by birth year of the author",
+      x = "Year of birth", 
+      y = "Relative occurrence", 
+      color = "Genre") +
+  theme_light() +
+  scale_colour_discrete(labels = c("Fantasy", "Non fiction", "Observational comedy", "Poetry", "Romance novel", "Satire", "Science fiction")) +
+  geom_vline(xintercept = 1927, linetype = "dashed", color = 'grey50') +
+  geom_vline(xintercept = 1945, linetype = "dashed", color = 'grey50') +
+  geom_vline(xintercept = 1964, linetype = "dashed", color = 'grey50') +
+  geom_vline(xintercept = 1980, linetype = "dashed", color = 'grey50') +
+  geom_vline(xintercept = 1996, linetype = "dashed", color = 'grey50') +
+  geom_point(alpha = 0.3) +
+  geom_smooth(se = FALSE)
+ggsave("Relative occurrence by genre.pdf", width = 10, height = 6)
