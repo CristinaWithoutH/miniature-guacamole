75b8d2d01f2c3f2f069358a60ee42e6e6b5d8f40
Malou Veen van Wolfswinkel <m.e.veenvanwolfswinkel@students.uu.nl>
2026-01-20 17:13:07 +0100

    Fixed converter and writing csv files with results

--- table_converter.py
+++ table_converter.py
@@ -15,16 +15,44 @@ year_dict = {}
 # genre_counter = Counter()
 for line in yearly_genres:
     line = line.split(",")
-    year = line[1]
-    genre = line[2].strip("/\n")
+    year = line[1].strip(' ')
+    genre = line[2].strip("/\n").strip(' ')
     if year not in year_dict:
-        year_dict[year] = {}
-    else:
-        if genre not in year_dict[year]:
-            year_dict[year][genre] = 1 
-        else:
-            year_dict[year][genre] += 1
+        year_dict[year] = {
+            'Fantasy' : 0,
+            'Science fiction' : 0,
+            'Observational comedy' : 0,
+            'Satire' : 0,
+            'Poetry' : 0,
+            'Non-fiction' : 0,
+            'Romance novel' : 0
+        }
+    year_dict[year][genre] += 1
 
+# relative choice occurence values
+yearly_relative_dict = {}
+for year in year_dict:
+    count_yearly = 0                    # total authors of that year
+    current_year_dict = year_dict[year]
+    for genre in current_year_dict:
+        count_yearly += current_year_dict[genre]
+    if year not in yearly_relative_dict:
+            yearly_relative_dict[year] = {
+                'Fantasy' : 0,
+                'Science fiction' : 0,
+                'Observational comedy' : 0,
+                'Satire' : 0,
+                'Poetry' : 0,
+                'Non-fiction' : 0,
+                'Romance novel' : 0
+            }
+    for genre in current_year_dict:
+        relative_genre = current_year_dict[genre] / count_yearly
+        yearly_relative_dict[year][genre] = relative_genre
+print(yearly_relative_dict)
 
-print(year_dict)
-
+# Writing to CSV file
+with open('yearly_genres.csv', 'w', encoding = 'utf-8') as file:
+    file.write('year, fantasy, science_fiction, poetry, satire, observational_comedy, non_fiction, romance_novel \n')
+    for year in yearly_relative_dict:
+        file.write(f'{year}, {yearly_relative_dict[year]['Fantasy']}, {yearly_relative_dict[year]['Science fiction']}, {yearly_relative_dict[year]['Poetry']}, {yearly_relative_dict[year]['Satire']}, {yearly_relative_dict[year]['Observational comedy']}, {yearly_relative_dict[year]['Non-fiction']}, {yearly_relative_dict[year]['Romance novel']}\n')
